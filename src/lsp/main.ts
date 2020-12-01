f3dc1df8a8ddc56bbd0e21ebc2bf495f95b2b3b3
Renato Xavier <rnatu91@gmail.com>
2020-12-01 15:03:10 -0300

    TypeScript - Princípios SOLID - LSP - Liskov substitution principle
    Aula sobre a utilização do Princípio da Substituição de Liskov

---
+++ src/lsp/main.ts
@@ -0,0 +1,40 @@
+/*
+Liskov substitution principle (Princípio da substituição de Liskov) -
+Se ϕ(x) é uma propriedade demonstrável dos objetos x de tipo T. Então ϕ(y)
+deve ser verdadeiro para objetos y de tipo S onde S é um subtipo de T.
+
+Mais simples: Subtipos precisam ser substituíveis por seus tipos de base.
+Mais simples ainda: Se meu programa espera um Animal, algo do tipo Cachorro (que herda de Animal) deve servir como qualquer outro Animal.
+*/
+
+//dependencias
+import { Messaging } from './services/messaging';
+import { Persistency } from './services/persistency';
+import { ShoppingCart } from './classes/shopping-cart';
+import { Product } from './classes/product';
+
+//classe
+import { Order } from './classes/order';
+import { NoDiscount } from './classes/discount';
+
+//dependencias
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+
+//injeção de dependencias no construtor
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Camiseta', 49.9));
+shoppingCart.addItem(new Product('Caderno', 9.9));
+shoppingCart.addItem(new Product('Lápis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
